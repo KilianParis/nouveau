bd721f1cd0049ea9ee5586df44ae68c73f9ddfae
KilianParis <parisk@saintjo.org>
2016-10-06 16:54:01 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<head>
+<title>BONJOUR!!!!!!!!</title>
+<meta charset="utf-8">
+<style type="text/css">
+body{
+	background-color: #336bff ;
+	color: green;
+}
+
+h1{
+	position: absolute;
+	top: auto;
+	width: 100%;
+	text-align: center;
+	font-family: Helvetica;
+	font-size: 100;
+}
+</style>
+</head>
+<boby>
+<h1>
+Bonjour le monde de git
+</h1>
+</boby>
